33a2067efab519364dad495331286a397905dcc4
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-12 09:40:43 -0600

    Added router path for github pages.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar/navbar';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 // import Stock from './pages/Stock';
 
 function App() {
+  const BASE_NAME = "/Stock-Up"
   return (
-    <div>
-      <Navbar/>
-    </div>
+      <Router basename={`${BASE_NAME}`}>
+        <div>
+          <Navbar/>
+        </div>
+      </Router>
   );
 }
 
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -5,10 +5,10 @@ function Navbar() {
     return (
         <nav className="navbar navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Navbar</a>
+                <h1>Stock Up</h1>
                 <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    <input className="form-control me-2" type="search" placeholder="Search Stock" aria-label="Search" />
+                        <button className="btn btn-outline-primary" type="submit">Search</button>
                 </form>
             </div>
         </nav>    )

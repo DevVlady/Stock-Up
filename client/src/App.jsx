15f1166257d2b3b33966ddb78276f43f5cf91799
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-12 16:40:29 -0600

    Added all the paths in the app.jsx file.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,40 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Signup from './pages/SignUp';
+import Login from './pages/Login';
+import Welcome from './pages/Welcome';
+import Dashboard from './pages/Dashboard';
+import NoMatch from './pages/NoMatch';
 import SignupForm from './components/SignUpForm/Form';
 // import Stock from './pages/Stock';
 
 function App() {
   const BASE_NAME = "/Stock-Up"
   return (
-      <Router basename={`${BASE_NAME}`}>
-        <div>
-          <Route exact path={["/", "/signup"]}>
+    <Router basename={`${BASE_NAME}`}>
+      <div>
+        <Switch>
+          <Route exact path={["/", "/welcome"]}>
+            <Welcome />
+          </Route>
+          <Route exact path="/signup">
             <Signup />
           </Route>
-          <SignupForm/>
-        </div>
-      </Router>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route exact path="/nomatch">
+            <NoMatch />
+          </Route>
+          <SignupForm />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

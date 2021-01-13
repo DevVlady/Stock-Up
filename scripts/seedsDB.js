c78dc9d1a3691ff72ccb039fa1155e41d4fbda3b
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-12 20:26:07 -0600

    Added some of the date for the seeds needed.

--- scripts/seedsDB.js
+++ scripts/seedsDB.js
@@ -1,2 +1,46 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/stock-up-users"
+);
+
+const userSeed = [
+    {
+        email: 'mikeb@gmail.com',
+        firstName: 'Mike',
+        lastName: 'B',
+        date: new Date(Date.now())
+    },
+    {
+        email: 'vv@gmail.com',
+        firstName: 'Vlad',
+        lastName: 'D',
+        date: new Date(Date.now())
+    },
+    {
+        email: 'josh@gmail.com',
+        firstName: 'Josh',
+        lastName: 'B',
+        date: new Date(Date.now())
+    },
+    {
+        email: 'jk@gmail.com',
+        firstName: 'Jason',
+        lastName: 'B',
+        date: new Date(Date.now())
+    },
+    {
+        email: 'lk@gmail.com',
+        firstName: 'Luke',
+        lastName: 'B',
+        date: new Date(Date.now())
+    },
+    {
+        email: 'ha@gmail.com',
+        firstName: 'Trump',
+        lastName: 'D',
+        date: new Date(Date.now())
+    }
+]

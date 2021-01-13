7b1f031f4ea68177570c9b2f51d8309ef2731e84
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-13 13:20:39 -0600

    Added the get user function insde the of the app.jsx file.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,6 +32,27 @@ class App extends Component {
     this.setState(userObject)
   }
 
+  getUser() {
+    axios.get('/user/').then(response => {
+      console.log('Get user response: ')
+      console.log(response.data)
+      if (response.data.user) {
+        console.log('Get User: There is a user saved in the server session: ')
+
+        this.setState({
+          loggedIn: true,
+          username: response.data.user.username
+        })
+      } else {
+        console.log('Get user: no user');
+        this.setState({
+          loggedIn: false,
+          username: null
+        })
+      }
+    })
+  }
+
   render() {
     return (
       // <Router basename={`${BASE_NAME}`}>

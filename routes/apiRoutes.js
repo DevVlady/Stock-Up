6dcd62989a36070a3f018b1ee2a59a09e323d666
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-13 14:30:30 -0600

    Grab existing users if the user is already built in the database.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -58,4 +58,15 @@ router.post(
     }
 )
 
+//Grab Exisiting Users
+router.get('/', (req, res, next) => {
+    console.log('===== user!!======')
+    console.log(req.user)
+    if (req.user) {
+        res.json({ user: req.user })
+    } else {
+        res.json({ user: null })
+    }
+})
+
 module.exports = router;

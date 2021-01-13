656a03ae0df30e5b370e348bddcf38a0943db8f5
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-13 13:49:43 -0600

    Added the bcrypt variable to the user.js inside the models folder.

--- models/user.js
+++ models/user.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+//
+const bcrypt = require('bcryptjs');
+mongoose.promise = Promise
+
 const userSchema = new Schema(
     {
         email: {
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,9 +6,9 @@ const path = require('path');
 // router.use("/api", apiRoutes);
 
 
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 
 module.exports = router;

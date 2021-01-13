4fa56adf990c39840090b221104cc5c1a831fc3c
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-13 14:06:15 -0600

    Changed the database to match what our stock users database is named.

--- client/database/index.js
+++ client/database/index.js
@@ -4,12 +4,12 @@ mongoose.Promise = global.Promise
 
 //your local database url
 //27017 is the default mongoDB port
-const uri = 'mongodb://localhost:27017/simple-mern-passport'
+const uri = 'mongodb://localhost:27017/stock-up-users'
 
 mongoose.connect(uri).then(
     () => {
         /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
-        console.log('Connected to Mongo');
+        console.log('Connected to MongoDB');
 
     },
     err => {

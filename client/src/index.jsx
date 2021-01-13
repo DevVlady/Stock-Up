e5d7b3ccaaf89f9f6b935407d14c9b8acd12dcb9
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-13 13:42:08 -0600

    Changed the component in the index.jsx file from ReactRouter to the BrowserRouter.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 //Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,9 +10,9 @@ import 'spectre.css/dist/spectre.min.css';
 import 'spectre.css/dist/spectre-icons.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 

a95e688c86cd29b67414f010bc4433282fe6c02c
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-13 14:25:20 -0600

    required all the neccessary packages and folders to the routes file to build the routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const path = require('path');
 // const apiRoutes = require("./api");
+const express = require('express');
+const User = require('../models/user');
+const passport = require('../passport')
 
 // API Routes
 // router.use("/api", apiRoutes);

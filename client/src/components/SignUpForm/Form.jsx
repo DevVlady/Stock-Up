0a07536ab44c5c0b3ceab617cc6b6cee8d6e376c
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-13 11:53:33 -0600

    Removed the old signup form and going to change the function to work a little differently.

--- client/src/components/SignUpForm/Form.jsx
+++ client/src/components/SignUpForm/Form.jsx
@@ -2,41 +2,18 @@ import React, { Component } from 'react';
 import './Form.css';
 import axios from 'axios';
 
-const SignupForm = () => {
-    return (
-        <div>
-            <div className="container">
-                <div className="row center">
-                    <div className="col-md-6 col-md-offset-3">
-                        <h2>Sign Up Form</h2>
-                        <form className="signup">
-                            <div className="form-group">
-                                <label for="email-input">Email address</label>
-                                <input type="email" className="form-control" id="email-input" placeholder="Email" />
-                            </div>
-                            <div className="form-group">
-                                <label for="firstName-input">First Name</label>
-                                <input type="firstName" className="form-control" id="firstName-input" placeholder="First Name" />
-                            </div>
-                            <div className="form-group">
-                                <label for="lastName-input">Last Name</label>
-                                <input type="lastName" className="form-control" id="lastName-input" placeholder="Last Name" />
-                            </div>
-                            <div id="alert" className="alert alert-danger display2" role="alert">
-                                <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                                <span className="sr-only">Error:</span> <span className="msg"></span>
-                            </div>
-                            <button type="submit" id="submitBtn" className="btn btn-default">Sign Up</button>
-                            OR
-                            <button type="submit" className="btn btn-default"><a href="/login/github">Use GitHub</a></button>
-                        </form>
-                        <br />
-                        <p>Or log in <a href="/login">here</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+class SignupForm extends Component {
+    constructor() {
+		super()
+		this.state = {
+			username: '',
+			password: '',
+			confirmPassword: '',
+
+		}
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
 }
 
 export default SignupForm;

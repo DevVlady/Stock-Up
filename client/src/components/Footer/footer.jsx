7ce3c9543cc5bba678b8b3de1c1c8586376a2761
Hoybaby <mbartek436@gmail.com>
2021-01-15 17:29:58 -0500

    attempting to bring in a react component I think  be very good. just a simple footer

---
+++ client/src/components/Footer/footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { makeStyles } from "@material-ui/core/styles";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
+// core components
+import styles from "assets/jss/material-dashboard-react/components/footerStyle.js";

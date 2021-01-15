5f74c39f5361eb7728bc1ef94c1d8bfc47432d83
Hoybaby <mbartek436@gmail.com>
2021-01-15 17:32:39 -0500

    did the standard exporting function so I can just apply the footer at the bottom

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -5,3 +5,15 @@ import ListItem from "@material-ui/core/ListItem";
 import List from "@material-ui/core/List";
 // core components
 import styles from "assets/jss/material-dashboard-react/components/footerStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Footer(props) {
+    const classes = useStyles();
+    return (
+        <footer>
+
+        
+        </footer>
+    )
+}

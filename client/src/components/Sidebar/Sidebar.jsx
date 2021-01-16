f83fcdf0cc39a6f4792a1b8bceb1c3f41973ce16
Hoybaby <mbartek436@gmail.com>
2021-01-15 23:20:56 -0500

    styling up the sidebar so our project can have something functional

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -5,21 +5,21 @@ import {SidebarData} from './SidebarData';
 function Sidebar() {
     return (
         <div className="Sidebar">
-            <ul>
+            <ul className="SidebarList">
             {SidebarData.map((val,key)=> {
                 return  (
-                <li key={key} onClick={() =>{window.location.pathname = val.link}}>
+                <li key={key} className="row" onClick={() =>{window.location.pathname = val.link}}>
                     {""}
                     <div>{val.icon}</div> {""}
                     <div>
                         {val.title}
                     </div>
-                    </li>
-                )
+                </li>
+                );
             })}
             </ul>
         </div>
-    )
+    );
 }
 
 export default Sidebar;

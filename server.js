466d83b2d9c39715a9ad07d726c28f536108104a
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-16 13:02:22 -0600

    NP

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(passport.session()) // calls the deserializeUser
 
 app.use(apiRoutes)
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, "./client/public/index.html"));
 })
 

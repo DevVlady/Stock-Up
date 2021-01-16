bda25a9381688e6db8fd7a13b2fa5e5fb8f3866f
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-15 18:07:45 -0600

    Added the final function to the logout.jsx fikle that allows the user to signout but the only way this works is using the logout path in the address bar. Worked on this with Mike B.

--- client/src/components/Logout/Logout.jsx
+++ client/src/components/Logout/Logout.jsx
@@ -2,18 +2,19 @@ import axios from 'axios';
 import React from 'react';
 // import { Button } from 'reactstrap';
 // import {Redirect} from "react-dom"
-import { Redirect } from "react-router-dom";
+// import { Redirect } from "react-router-dom";
 import { Button } from 'reactstrap';
 import { useHistory } from "react-router-dom";
 
 
 const Logout = () => {
+    let history = useHistory();
     const logout = () => {
-        axios().then(res => history.push("/login"))
+        axios('/logout').then(res => history.push("/login"))
       }
     return (
         <div>
-            <Button color="danger">danger</Button>{' '}
+            <Button onClick={() => logout()}>Logout</Button>
         </div>
     )
 }

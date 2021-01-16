0e504f9be7e3efa79362d7e0e53267f0aea8f920
Hoybaby <mbartek436@gmail.com>
2021-01-15 22:40:08 -0500

    making some changes to the navbar where i want it to be

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./Sidebar.css"
+import {SidebarData} from './SidebarData';
 
 function Sidebar() {
     return (
         <div className="Sidebar">
-            hello
+            <ul>
+            {SidebarData.map((val,key)=> {
+                return  (
+                <li key={key}></li>
+                )
+            })}
+            </ul>
         </div>
     )
 }

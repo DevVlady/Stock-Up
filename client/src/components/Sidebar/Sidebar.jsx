2d7f62471c19043b89f67f1d3f70c1a0cc4cc0ff
Hoybaby <mbartek436@gmail.com>
2021-01-15 20:38:22 -0500

    starting from scratch to get sidebar to work properly

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,41 +1,13 @@
 import "./sidebar.css";
 import React from "react";
 
-const Sidebar = ({ width, height, children }) => {
-    const [xPosition, setX] = React.useState(-width);
+ export const Sidebar = ({ width, height, children }) => {
+    
 
-    const toggleMenu = () => {
-        if (xPosition < 0) {
-            setX(0);
-        } else {
-            setX(-width);
-        }
-    };
-
-    React.useEffect(() => {
-        setX(0);
-    }, []);
-    return (
-        <React.Fragment>
-            <div
-                className="side-bar"
-                style={{
-                    transform: `translatex(${xPosition}px)`,
-                    width: width,
-                    minHeight: height
-                }}
-            >
-                <button
-                    onClick={() => toggleMenu()}
-                    className="toggle-menu"
-                    style={{
-                        transform: `translate(${width}px, 20vh)`
-                    }}
-                ></button>
-                <div className="content">{children}</div>
-            </div>
-        </React.Fragment>
-    );
-};
-
-export default Sidebar;
+    return(
+        <div className="side-bar" style={{width: width, minHeigh: height}}>
+            <React.FRagment>{children}</React.FRagment>
+        </div>
+    )
+}
+    

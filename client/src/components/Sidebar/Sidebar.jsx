30fc831db66d7bb5dfc4cb7740ccce0abf2db066
Hoybaby <mbartek436@gmail.com>
2021-01-16 00:01:11 -0500

    everything is going ok

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -8,13 +8,14 @@ function Sidebar() {
             <ul className="SidebarList">
             {SidebarData.map((val,key)=> {
                 return  (
-                <li key={key} className="row" onClick={() =>{window.location.pathname = val.link}}>
-                    {""}
-                    <div>{val.icon}</div> {""}
-                    <div>
-                        {val.title}
-                    </div>
-                </li>
+                    <li 
+                    key={key} 
+                    className="row" 
+                    onClick={() =>{
+                        window.location.pathname = val.link}}>
+                        
+                        <div id="icon">{val.icon}</div>.<div id="title">{val.title}</div>
+                    </li>
                 );
             })}
             </ul>

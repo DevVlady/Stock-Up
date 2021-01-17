1489c81f7b010c43a9d83763adc96212ec12590c
Hoybaby <mbartek436@gmail.com>
2021-01-17 13:18:08 -0500

    imported icons from react icons to later use in my navbar

---
+++ client/src/components/Navbar2/Navbar2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+
+function Navbar() {
+    <div>
+        <div className="navbar">
+            <Link to="#" className='menu-bars'>
+
+            </Link>
+        </div>
+    </div>
+}

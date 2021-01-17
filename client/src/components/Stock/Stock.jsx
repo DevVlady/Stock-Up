20dced6072b849cac321e25def148d9b3900554d
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-17 14:32:04 -0600

    Added the constructor to displand insterted the x and y values for the chart. Also added the fecthStock skeleton code which needs additional functions to operate properly.

--- client/src/components/Stock/Stock.jsx
+++ client/src/components/Stock/Stock.jsx
@@ -1,6 +1,22 @@
+import { use } from 'passport';
 import React from 'react';
 
 class Stock extends React.Component {
+    constructor(props) {
+        super(props);
+        use.state = {
+            stockChartXValues: [],
+            stockChartYValue: []
+        }
+    }
+
+    componentDidMount() {
+        this.fetchStock();
+    }
+
+    fetchStock() {
+        
+    }
 
     render() {
         return(

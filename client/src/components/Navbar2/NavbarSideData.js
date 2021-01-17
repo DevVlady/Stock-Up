6986b18cd92549b951470c54218277b98ed69fa0
Hoybaby <mbartek436@gmail.com>
2021-01-17 13:51:19 -0500

    added all the data and objects i need for my icons in my navbar/sidebar with the appriote icons

--- client/src/components/Navbar2/NavbarSideData.js
+++ client/src/components/Navbar2/NavbarSideData.js
@@ -5,6 +5,33 @@ import * as IoIcons from 'react-icons/io';
 
 export const SidebarData = [
     {
-        
-    }
+        title: "Home",
+        path: '/',
+        icon: <AiIcons.AiFillHome/>,
+        cName: 'nav-text'
+    }, 
+    {
+        title: "Graphs",
+        path: '/',
+        icon: <AiIcons.AiOutlineBarChart/>,
+        cName: 'nav-text'
+    }, 
+    {
+        title: "News",
+        path: '/',
+        icon: <FaIcons.FaRegNewspaper/>,
+        cName: 'nav-text'
+    },
+    {
+        title: "Contact Us",
+        path: '/',
+        icon: <FaIcons.FaEnvelopeOpenText />,
+        cName: 'nav-text'
+    },
+    {
+        title: "Logout",
+        path: '/',
+        icon: <AiIcons.AiFillHome/>,
+        cName: 'nav-text'
+    },
 ]

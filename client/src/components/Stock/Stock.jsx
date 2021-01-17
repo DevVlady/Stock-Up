fb19333bd1b81a8cc139a604c987a1416aab49d1
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-17 14:02:05 -0600

    Finished the starting part of the dstock function and going to work on the page which will be used to display the stock charts in the app.js using path method.

--- client/src/components/Stock/Stock.jsx
+++ client/src/components/Stock/Stock.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 class Stock extends React.Component {
 
+    render() {
+        return(
+            <div>
+                <h1>Stock Market</h1>
+            </div>
+        )
+    }
 }
 
 export default Stock;

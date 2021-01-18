97f5bb78a9a80ab38a6200ccf0dc67f12e84997f
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-18 16:23:21 -0600

    Styled the login page to have thlogin box position on the center of the page.

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -59,30 +59,32 @@ class LoginForm extends Component {
             return <Redirect to={{ pathname: this.state.redirectTo }} />
         } else {
             return (
-                <Form inline>
-                    {/* <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                <div className="loginForm">
+                    <Form inline>
+                        {/* <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
                         <Label for="First Name" className="mr-sm-2">First Name</Label>
                         <Input type="text" name="firstName" id="firstName" value={this.state.firstName} onChange={this.handleChange}/>
                     </FormGroup> */}
-                    {/* <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                        {/* <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
                         <Label for="Last Name" className="mr-sm-2">Last Name</Label>
                         <Input type="text" name="lastName" id="lastName" value={this.state.lastName} onChange={this.handleChange}/>
                     </FormGroup> */}
-                    <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                        <Label for="Username" className="mr-sm-2">Username</Label>
-                        <Input type="text" name="username" id="username" placeholder="Username" value={this.state.username} onChange={this.handleChange}/>
-                    </FormGroup>
-                    {/* <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                            <Label for="Username" className="mr-sm-2">Username</Label>
+                            <Input type="text" name="username" id="username" placeholder="Username" value={this.state.username} onChange={this.handleChange} />
+                        </FormGroup>
+                        {/* <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
                         <Label for="email" className="mr-sm-2">Email</Label>
                         <Input type="email" name="email" id="email" placeholder="email@example.com" value={this.state.email} onChange={this.handleChange}/>
                     </FormGroup> */}
-                    <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                        <Label for="Password" className="mr-sm-2">Password</Label>
-                        <Input type="password" name="password" id="Password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
-                    </FormGroup>
-                    <Button onClick={this.handleSubmit}>Submit</Button>
-                    <a href="/signup">Signup</a>
-                </Form>
+                        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                            <Label for="Password" className="mr-sm-2">Password</Label>
+                            <Input type="password" name="password" id="Password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
+                        </FormGroup>
+                        <Button onClick={this.handleSubmit}>Submit</Button>
+                        <a href="/signup">Signup</a>
+                    </Form>
+                </div>
             )
         }
     }

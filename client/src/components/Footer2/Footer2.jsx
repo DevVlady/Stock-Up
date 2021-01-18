14f469f92670aac4d6844b1ed49098be79322f65
Hoybaby <mbartek436@gmail.com>
2021-01-17 21:43:00 -0500

    changing the names in the footer2 for the proper ways for the rest

--- client/src/components/Footer2/Footer2.jsx
+++ client/src/components/Footer2/Footer2.jsx
@@ -8,3 +8,19 @@ export default function Footer({children, ...restProps}) {
 Footer.Wrapper = function FooterWrapper({children,...restProps}) {
     return <Wrapper {...restProps}>{children}</Wrapper>
 }
+
+Footer.Row = function FooterRow({children,...restProps}) {
+    return <Row {...restProps}>{children}</Row>
+}
+
+Footer.Column = function FooterColumn({children,...restProps}) {
+    return <Column {...restProps}>{children}</Column>
+}
+
+Footer.Link = function FooterLink({children,...restProps}) {
+    return <Link {...restProps}>{children}</Link>
+}
+
+Footer.Title = function FooterTitle({children,...restProps}) {
+    return <Title {...restProps}>{children}</Title>
+}

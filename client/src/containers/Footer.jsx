e45136b3b2fc2607442898cf88c80746b029f824
Hoybaby <mbartek436@gmail.com>
2021-01-17 23:10:19 -0500

    added more columns in the footer section howeer they are stacked on top of each other

--- client/src/containers/Footer.jsx
+++ client/src/containers/Footer.jsx
@@ -12,7 +12,19 @@ export function FooterContainer() {
                     <Footer.Link href="#">Clients</Footer.Link>
                     <Footer.Link href="#">Testimonials</Footer.Link>
                 </Footer.Column>
+                <Footer.Column>
+                    <Footer.Title>Services</Footer.Title>
+                    <Footer.Link href="#">Story</Footer.Link>
+                    <Footer.Link href="#">Clients</Footer.Link>
+                    <Footer.Link href="#">Testimonials</Footer.Link>
+                </Footer.Column>
             </Footer.Wrapper>
+            <Footer.Column>
+                    <Footer.Title>Developers</Footer.Title>
+                    <Footer.Link href="#">Michael Bartek</Footer.Link>
+                    <Footer.Link href="#">Vladimir Dimitrov</Footer.Link>
+                    <Footer.Link href="#">Joshua Obando</Footer.Link>
+                </Footer.Column>
         </Footer>
 
         )

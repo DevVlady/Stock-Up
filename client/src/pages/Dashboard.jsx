98e3af4661e2a5e1ab6726c9aaa3efc09d76f9a4
Hoybaby <mbartek436@gmail.com>
2021-01-18 22:36:46 -0500

    commiting changes so i can pull from main

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,37 +1,11 @@
 import React from 'react';
-import Navbar from '../components/Navbar/Navbar';
-// import Jumbotron from '../components/Jumbotron/Jumbotron';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-// import Footer from '../components/Footer/Footer';
-import Stocks from '../pages/Stocks';
-import Logout from '../components/Logout/Logout'
-<<<<<<< HEAD
-// import {FooterContainer} from '../containers/Footer'
-=======
-import Login from '../components/Login/Login'
-import {FooterContainer} from '../containers/Footer'
->>>>>>> b63ea3570ac543a98f0f8172cc619c772c7a6cb1
 
 const Dashboard = (props) => {
     return (
-        <Router>
-            <Navbar/>
-            <Switch>
-                <Route exact path="/stocks" component={Stocks}/>
-                <Route exact path="/logout" component={Logout}/>
-<<<<<<< HEAD
-            </Switch>
-            <Jumbotron/>
-            {/* <FooterContainer/> */}
-=======
-                <Route exact path="/login" component={Login}/>
-            </Switch>
-            {/* <Jumbotron/> */}
-            <FooterContainer/>
->>>>>>> b63ea3570ac543a98f0f8172cc619c772c7a6cb1
-        </Router>
-    )
+       <div>
 
+       </div>
+    )
 };
 
 export default Dashboard;

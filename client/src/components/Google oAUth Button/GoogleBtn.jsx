497f0caceb6584a36af6784e196ba8d5f915ccdf
Hoybaby <mbartek436@gmail.com>
2021-01-18 23:17:24 -0500

    finished the code and the button should work just fine

--- client/src/components/Google oAUth Button/GoogleBtn.jsx
+++ client/src/components/Google oAUth Button/GoogleBtn.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import GoogleLogin from 'react-google-login';
 
 export class GoogleBtn extends Component {
+
+    responseGoogle=(response)=> {
+        console.log(response);
+        console.log(response.profileObj);
+    }
+
     render() {
         return (
             <div>

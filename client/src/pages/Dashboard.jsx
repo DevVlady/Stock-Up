8363575c17299d923989814e40f827f0e7869df0
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-19 12:13:50 -0600

    Moved the login button from the dashboard to the signup page git add .

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,26 +1,13 @@
 import React, { Component } from 'react'
-import GoogleLogin from 'react-google-login';
 
-let loginPath = (process.env.NODE_ENV === "production") ? "https://my-stock-up.herokuapp.com/auth/google" : "http://localhost:3001/auth/google"
 
 export class Dashbooard extends Component {
 
-    responseGoogle=(response)=> {
-        console.log(response);
-        console.log(response.profileObj);
-    }
 
     render() {
         return (
             <div>
-                <GoogleLogin
-                clientId="984123359552-m0q70i6c34gfb986tk9bbvobjh9k8huq.apps.googleusercontent.com"
-                buttonText="Log In"
-                href={loginPath}
-                onSuccess={this.responseGoogle}
-                onFailure={this.responseGoogle}
-                cookiePolicy={'single_host_origin'}
-                />
+
             </div>
         )
     }

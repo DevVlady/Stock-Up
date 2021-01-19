fa25aa87d517a1be3353d723a899a60aa57c4ec2
Hoybaby <mbartek436@gmail.com>
2021-01-19 12:01:27 -0500

    working on the google oAUth with the ta and got a good portion of it done. Passing the torch to Vlad to finish it up

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import GoogleLogin from 'react-google-login';
 
+let loginPath = (process.env.NODE_ENV === "production") ? "" : "http://localhost:3001/auth/google"
+
 export class Dashbooard extends Component {
 
     responseGoogle=(response)=> {
@@ -14,6 +16,7 @@ export class Dashbooard extends Component {
                 <GoogleLogin
                 clientId="984123359552-m0q70i6c34gfb986tk9bbvobjh9k8huq.apps.googleusercontent.com"
                 buttonText="Log In"
+                href={loginPath}
                 onSuccess={this.responseGoogle}
                 onFailure={this.responseGoogle}
                 cookiePolicy={'single_host_origin'}

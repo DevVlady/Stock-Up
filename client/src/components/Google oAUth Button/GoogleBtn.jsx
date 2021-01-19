dd51aab9e9e3bf7d98b23c68a1154f5dda872879
Hoybaby <mbartek436@gmail.com>
2021-01-18 23:12:18 -0500

    finished the rest of the div

--- client/src/components/Google oAUth Button/GoogleBtn.jsx
+++ client/src/components/Google oAUth Button/GoogleBtn.jsx
@@ -5,10 +5,16 @@ export class GoogleBtn extends Component {
     render() {
         return (
             <div>
-                
+                <GoogleBtn
+                clientId="984123359552-m0q70i6c34gfb986tk9bbvobjh9k8huq.apps.googleusercontent.com"
+                buttonText=""
+                onSuccess={this.responseGoogle}
+                onFailure={this.responseGoogle}
+                cookiePolicy={'single_host_orgin'}
+                />
             </div>
         )
     }
 }
 
-export default GoogleBtn
+export default GoogleBtn;

f1d66f44d0b1ce3dbf49ffd9a1911b5e731b830a
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-18 23:17:41 -0600

    Styled the welcome page and added a slogan.

--- client/src/components/Welcome/Welcome.jsx
+++ client/src/components/Welcome/Welcome.jsx
@@ -4,15 +4,36 @@ import Logo from './images/Stock_Up_Logo.png'
 import { Jumbotron, Button } from 'reactstrap';
 
 
+var style = {
+  base: {
+      background: '#060b26',
+      border: 'none',
+      fontSize: '30px',
+      marginTop: '5px',
+      marginBottom: '5px',
+      marginLeft: '44%',
+      color: 'red'
+  },
+  tron: {
+      heigth: '1100px',
+      background: '#2980b9'
+  },
+  head: {
+    textAlign: 'center',
+    marginBottom: '5px'
+}
+};
+
+
 const Welcome = (props) => {
   return (
     <div>
-      <Jumbotron>
-        <h1 className="display-3">The FUTURE is here</h1>
+      <Jumbotron style={style.tron}>
         <img src={Logo} alt="Logo"/>
-        <hr className="my-2" />
+        <h1 style={style.head} className="display-3">FUTURE...INVESTED</h1>
+
         <p className="lead">
-          <a href="/signup"><Button style={{fontSize: '25px', alignItems: 'center'}} color="primary">Sign Up</Button></a>
+          <a href="/signup"><Button style={style.base} color="primary">Sign Up</Button></a>
         </p>
       </Jumbotron>
     </div>

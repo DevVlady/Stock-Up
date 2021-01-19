5d04d13a8b6c722a0ec00c43f676f34455351c22
Hoybaby <mbartek436@gmail.com>
2021-01-18 23:34:33 -0500

    didnt work so trying a new method

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import GoogleBtn from '../components/Google oAUth Button/GoogleBtn';
 
 
 
 const Dashboard = (props) => {
     return (
         <div>
-
+            <GoogleBtn/>
         </div>
     )
 }

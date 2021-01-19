6fc7acc686fee50ebf2c2e33bfd5895637e9b90e
Hoybaby <mbartek436@gmail.com>
2021-01-18 23:09:56 -0500

    adding the code to get the google button

---
+++ client/src/components/Google oAUth Button/GoogleBtn.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import GoogleLogin from 'react-google-login';
+
+export class GoogleBtn extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default GoogleBtn

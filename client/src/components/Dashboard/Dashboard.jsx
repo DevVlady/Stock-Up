160caa9a614bba69e5f336d7b54da354591517bf
Hoybaby <mbartek436@gmail.com>
2021-01-21 00:54:28 -0500

    was thinking of adding another card to this dashboard but instead of the card went to put a pie chart

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -15,13 +15,38 @@ const Main = () => {
 
                 <div className="main__cards">
                     <div className="card">
-                        <i
-                            className="fa fa-user-o fa-2x text-lightblue"
+                        <i className="fa fa-user-o fa-2x text-lightblue"
                             aria-hidden="true"></i>
-                            <div className="card_inner">
-                                <p className="text-primary-p"> Number of Stock Transactions a Min</p>
-                                <span className="font-bold text-title">578</span>
-                            </div>
+                        <div className="card_inner">
+                            <p className="text-primary-p"> Number of Stock Transactions a Min</p>
+                            <span className="font-bold text-title">578</span>
+                        </div>
+                    </div>
+                    {/* New card */}
+                    <div className="card">
+                        <i className="fa fa-calendar fa-2x text-red" aria-hidden="true"></i>
+                        <div className="card_inner">
+                            <p className="text-primary-p">Number of Stock Options</p>
+                            <span className="font-bold text-title">2467</span>
+                        </div>
+                    </div>
+                    {/* New Card */}
+                    <div className="card">
+                        <i className="fa fa-calendar fa-2x text-red" aria-hidden="true"></i>
+                        <div className="card_inner">
+                            <p className="text-primary-p">Year To Date Growth on Portfolio</p>
+                            <span className="font-bold text-title">2.1%</span>
+                        </div>
+                    </div>
+
+                    <div >
+                        {/* <i    className="fa fa-thumbs-up fa-2x text-green"
+                            aria-hidden="true"
+                        ></i> */}
+                        <div className="card_inner">
+                            <img src="https://static.twentyoverten.com/5a859cb98cd42f3a175b244a/0qcjBl_NTTZ/portfoliochart.png" className="stockPie" />
+                            <span className="font-bold text-title"></span>
+                        </div>
                     </div>
                 </div>
 

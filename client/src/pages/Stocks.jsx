d07be4e97a3cd534877c860b0460d88a76d95be4
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-20 23:46:06 -0600

    added the constructor in the marketstocks fucntion which will be used when trying to find the a stock by its symbol.

--- client/src/pages/Stocks.jsx
+++ client/src/pages/Stocks.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import Stock from '../components/Stock/Stock';
 import SearchBar from '../components/Search/Search';
+import axios from 'axios';
 
 
 
-const MarketStocks = (props) => {
-    return (
-        <div>
-        <SearchBar/>
-        <Stock/>
-        </div>
-    )
+class MarketStocks extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            stocks: [],
+            search: "",
+            // sortDirection: 0,
+        };
+    }
 
+    render(){
+        return (
+            <div>
+            <SearchBar/>
+            <Stock/>
+            </div>
+        )
+    }
 };
 
 export default MarketStocks;
